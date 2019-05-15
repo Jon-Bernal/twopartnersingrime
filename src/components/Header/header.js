59b8bb55fa1c7bddb3c3ebd620aba0728a39cac5
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-15 08:12:37 -0700

    began styling.  Rethinking styling and will be converting to a styling library for speed.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,18 +5,22 @@ import './header.scss';
 
 const Header = () => (
   <header>
-    <div className="logo">
-      <h1 className="logo-h1">
-        <Link to="/">Two Partners In Grime</Link>
-      </h1>
-      <p>Welcome To The Family</p>
+    <div className="container">
+      <div className="logo">
+        <h1 className="logo-h1">
+          <Link className="logo-link" to="/">Two Partners In Grime</Link>
+        </h1>
+        <p className="tag-line">Welcome To The Family</p>
+      </div>
     </div>
     <nav>
-      <Link className='nav-links' to="/">Home</Link>
-      <Link className='nav-links' to="/home-cleaning">Home Cleaning</Link>
-      <Link className='nav-links' to="/commercial-cleaning">Commercial Cleaning</Link>
-      <Link className='nav-links' to="/faq-page">FAQ</Link>
-      <Link className='nav-links' to="/contact-us">Contact Us</Link>
+      <div className="container">
+        <Link className='nav-links' to="/">Home</Link>
+        <Link className='nav-links' to="/home-cleaning">Home Cleaning</Link>
+        <Link className='nav-links' to="/commercial-cleaning">Commercial Cleaning</Link>
+        <Link className='nav-links' to="/faq-page">FAQ</Link>
+        <Link className='nav-links' to="/contact-us">Contact Us</Link>
+      </div>
     </nav>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import '../styles/reset.scss'
 
 import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
